6c5f0cbd2f1b032c7876e9a909deefbe7b19965a
broerjuang <juangwiantoro@gmail.com>
2016-11-17 13:18:48 +0700

    creating cms model

---
+++ server/controllers/cms.controller.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const CMS = require('../models/cms.model');
+
+let l
---
+++ server/models/cms.model.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Increment = require('mongoose-sequence')
+
+const cmsSchema = new Schema({
+  cms_id: {type: Number},
+  letter: {type: String},
+  frequency: {type: Number},
+  coming: {type: Date}
+})
+
+cmsSchema.plugin(Increment, {inc_field: 'cms_id'})
+module.exports = mongoose.model('CMS', cmsSchema)
