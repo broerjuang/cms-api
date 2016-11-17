22e598bdd140a3d355c816025feeca22af47031e
broerjuang <juangwiantoro@gmail.com>
2016-11-17 13:13:57 +0700

    creating routes

---
+++ server/routes/cms.route.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const router = expres.Router();
+const Controller = require('../controllers/cms.controller')
+
+// Get all cms data
+router.get('/cms', Controller.list)
+
+//  Post a single CMS
+router.post('/cms', Controller.generate)
+
+//  Update a single CMS
+router.put('/cms/:cms_id', Controller.update)
+
+//  Delete a single CMS
+router.delete('/cms/:cms_id', Controller.destroy)
