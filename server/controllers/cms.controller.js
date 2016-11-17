5b9570daa5b7b29d17ef405241fffc58be436f92
broerjuang <juangwiantoro@gmail.com>
2016-11-17 13:23:45 +0700

    cms controller is done

--- server/controllers/cms.controller.js
+++ server/controllers/cms.controller.js
@@ -2,4 +2,37 @@
 
 const CMS = require('../models/cms.model');
 
-let l
+let list = (req, res) => {
+  CMS
+    .find()
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err))
+}
+
+let generate = (req, res) => {
+  CMS
+    .create(req.body)
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err))
+}
+
+let update = (req, res) => {
+  CMS
+    .findOneAndUpdate({cms_id: req.params.cms_id})
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err))
+}
+
+let destroy = (req, res) => {
+  CMS
+    .findOneAndRemove({cms_id: req.params.cms_id})
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err))
+}
+
+module.exports = {
+  list,
+  generate,
+  update,
+  destroy
+}
