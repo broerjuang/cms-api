ad75de921b24a5383096693cb458c7bcd53753ce
broerjuang <juangwiantoro@gmail.com>
2016-11-17 14:39:31 +0700

    creating table for update and delete

--- client/src/javascript/script.js
+++ client/src/javascript/script.js
@@ -15,20 +15,20 @@ $(document).ready(() => {
             app.cms = item.data
             console.log(item)})
           .catch((err) => console.log(err))
+      },
+      postCms: function() {
+        axios
+          .post('http://localhost:3000/api/cms', {
+            letter: app.letter,
+            frequency: app.frequency,
+            coming: app.coming
+          })
+          .then((result) => {
+            app.cms.unshift(result)
+            console.log(result)
+          })
+          .catch((err) => {console.log(err)})
       }
-      // postCms: function() {
-      //   axios
-      //     .post('http://localhost:3000/api/cms', {
-      //       letter: app.letter,
-      //       frequency: app.frequency,
-      //       coming: app.coming
-      //     })
-      //     .then((result) => {
-      //       app.cms.unshift(result)
-      //       console.log(result)
-      //     })
-      //     .catch((err) => {console.log(err)})
-      // }
     }
   })
 
